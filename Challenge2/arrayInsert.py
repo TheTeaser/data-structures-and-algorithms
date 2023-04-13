5fd7f598393bd94428119d7b481289edacd633eb
TheTeaser <theteaser98@gmail.com>
2023-04-13 18:54:50 +0000

    Added code files for CC 2 & 3

--- Challenge2/arrayInsert.py
+++ Challenge2/arrayInsert.py
@@ -1,12 +1,24 @@
-#Did not use Snakecase in function name as the question stated to name the function like this.
+# Did not use Snakecase in function name as the question stated to name the function like this.
 
-def insertShiftArray(Arr, Val):
-    middle= len(Arr)//2     # it gave an error when I used a single / as we need an integer location, thus you need // to give a floor answer.
-    Arr.insert(middle, Val) #insert(poistion, value) can be used in lists and arrays.
-    print(Arr)
+# def insertShiftArray(Arr, Val):
+#     middle= len(Arr)//2     # it gave an error when I used a single / as we need an integer location, thus you need // to give a floor answer.
+#     Arr.insert(middle, Val) #insert(poistion, value) can be used in lists and arrays.
+#     print(Arr)
+
+# Redone the code to not include the insert function and not to use any library.
+def insertShiftArray(Arr, value):
+    middle = (len(Arr)+1) // 2
+    new_arr = [0] * (len(Arr)+1)
+    for i in range(middle):
+        new_arr[i] = Arr[i]
+    new_arr[middle] = value
+    for i in range(middle+1, len(Arr)+1):
+        new_arr[i] = Arr[i-1]
+    print(new_arr)
 
 
-insertShiftArray([2,4,6,-8],5)
+
+insertShiftArray([2,4,6,8],5)
 insertShiftArray([42,8,15,23,42],16)
 
 
