d66623074e14f91b78a170e9b97c775493a6decb
TheTeaser <theteaser98@gmail.com>
2023-05-22 17:03:29 +0000

    CC16 Done

--- Challenge15/trees.py
+++ Challenge15/trees.py
@@ -9,6 +9,13 @@ class Binary_Tree:
         self.root = None
             
     def pre_order(self, root=None, returned_list=None):
+
+
+        """
+        Perform pre-order traversal on the Binary Tree and return a list of values.
+        """
+
+
         if returned_list is None:
             returned_list = []
         if root is not None:
@@ -18,6 +25,9 @@ class Binary_Tree:
         return returned_list
 
     def in_order(self, root=None, returned_list=None):
+        '''
+        Perform in-order traversal on the Binary Tree and return a list of values.
+        '''
         if returned_list is None:
             returned_list = []
         if root is not None:
@@ -27,6 +37,9 @@ class Binary_Tree:
         return returned_list
 
     def post_order(self, root=None, returned_list=None):
+        '''
+        Perform post-order traversal on the Binary Tree and return a list of values.
+        '''
         if returned_list is None:
             returned_list = []
         if root is not None:
@@ -35,11 +48,36 @@ class Binary_Tree:
             returned_list.append(root.value)
         return returned_list
     
+    def find_maximum_value(self):
+
+        '''
+        Find the maximum value stored in the Binary Tree.
+        '''
+
+        if self.root is None:
+            raise ValueError("The tree is empty.")
+        return self.recursive_maximum_value(self.root)
+
+    def recursive_maximum_value(self, node):
+
+        '''
+        Recursively find the maximum value starting from the given node, Note this is a helper method for find_maximum_value().
+        '''
+        
+        if node.right is not None:
+            return self.recursive_maximum_value(node.right)
+        return node.value
+    
 class Binary_Search_Tree(Binary_Tree):
     def __init__(self):
         super().__init__()
 
     def Add(self, value):
+        
+        '''
+        Add a new node with the given value to the Binary Search Tree.
+        '''
+
         if not isinstance(value, int):
             raise ValueError("Value must be an integer.")
         if self.root is None:
@@ -48,6 +86,10 @@ class Binary_Search_Tree(Binary_Tree):
             self.recursion(self.root, value)
      
     def recursion(self, node, value):
+
+        '''
+        Recursively traverse the Binary Search Tree to find the appropriate position for the new node.
+        '''
         if value > node.value:
             if node.right is None:
                 node.right = Node(value)
@@ -63,11 +105,19 @@ class Binary_Search_Tree(Binary_Tree):
             raise ValueError("Node with the same value already exists in the tree.")
 
     def Contains(self, value):
+
+        '''
+        Check if a node with the given value exists in the Binary Search Tree.
+        '''
         if not isinstance(value, int):
             raise ValueError("Value must be an integer.")
         return self.recursive_search(self.root, value)
 
     def recursive_search(self, node, value):
+        
+        '''
+        Recursively search for a node with the given value starting from the given node.
+        '''
         if node is None:
             return False
 
